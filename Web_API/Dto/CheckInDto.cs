9a9f13b7149136e7a22a526e3c21d47d6d426993
Shongwe <senzosenkosi.s.shongwe@gmail.com>
2024-09-30 13:06:19 +0200

    dto's

---
+++ Web_API/Dto/CheckInDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Web_API.Dto
+{
+    public class CheckInDto
+    {
+        public int VehicleId { get; set; }
+        public int MileageAtReturn { get; set; }
+        public string FuelLevelAtReturn { get; set; } = string.Empty;
+        public string ConditionAtReturn { get; set; } = string.Empty;
+        public string ConditionNotesAtReturn { get; set; } = string.Empty;
+        public DateTime? PickUpDate { get; set; }
+        public DateTime? DropOffDate { get; set; }
+    }
+}
---
+++ Web_API/Dto/PaymentDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Web_API.Dto
+{
+    public class PaymentDto
+    {
+        public int PaymentId { get; set; }
+        public int RentalId { get; set; }
+        public DateTime? PaymentDate { get; set; }
+        public double AmountPaid { get; set; }
+        public string PaymentMethod { get; set; } = string.Empty;
+        public string PaymentStatus { get; set; } = string.Empty;
+    }
+}
